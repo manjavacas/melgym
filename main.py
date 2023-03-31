647a04279e88fcf1152a96e45708a0eb3f261a04
Antonio <antomanjavacas@gmail.com>
2023-03-31 15:18:36 +0200

    Preliminary step definition

--- main.py
+++ main.py
@@ -2,18 +2,27 @@
 import gymnasium as gym
 import melgym
 
+from stable_baselines3.dqn import DQN
+
 INPUT_FILE = './sample.inp'
 EDF_FILE = './VARIABLES.DAT'
 
 N_EPISODES = 1
 
+N_CVS = 1
+N_ACTIONS = 7
+
 def run():
 
-    env = gym.make('sample-v0', obs_size=1, n_actions=3)
+    env = gym.make('sample-v0', obs_size=N_CVS, n_actions=N_ACTIONS)
+
+    agent = DQN(env)
+
     for i in range(N_EPISODES):
         obs, _ = env.reset()
-        print(obs)
-
+        # (en main) [P1, P2, P3... Pk] -> NN -> [action1, action2, action3... actionN]
+        actions = agent.predict(obs)
+        next_obs, reward, done, _, _ = env.step(actions)
     
 
 if __name__ == '__main__':
---
+++ melgym/utils/rewards.py
@@ -0,0 +1,4 @@
+# TO-DO: recorrer presiones y ver cuantas tienen una variacion < K
+# Otra opcion: sumar las variaciones -> valor a minimizar
+def get_pressure_compliance(obs):
+    return NotImplementedError
