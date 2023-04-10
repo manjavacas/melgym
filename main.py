0979bd7cc5445cb8052e0b408b00ceb1ea5fc251
Antonio <antomanjavacas@gmail.com>
2023-04-10 13:30:46 +0200

    Added first version of supply CF manipulation

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def run():
 
     env = gym.make('sample-v0', obs_size=N_CVS, n_actions=N_ACTIONS)
 
-    agent = DQN(env)
+    # agent = DQN('MlpPolicy', env)
 
-    for i in range(N_EPISODES):
+    for _ in range(N_EPISODES):
         obs, _ = env.reset()
         # (en main) [P1, P2, P3... Pk] -> NN -> [action1, action2, action3... actionN]
-        actions = agent.predict(obs)
-        next_obs, reward, done, _, _ = env.step(actions)
-    
+        # actions = agent.predict(obs)
+        next_obs, reward, done, _, _ = env.step([4])
+        
 
 if __name__ == '__main__':
     run()
