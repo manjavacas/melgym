f2656faa645ca5cf1c4e72421082198adf86246d
Antonio <antomanjavacas@gmail.com>
2023-04-10 13:30:46 +0200

    Added first version of supply CF manipulation

--- melgym/envs/sample_env.py
+++ melgym/envs/sample_env.py
@@ -4,8 +4,11 @@ import numpy as np
 import os
 import subprocess
 
+from melkit.toolkit import Toolkit
+
 from gymnasium import spaces
 from ..utils import edf, rewards
+
 
 RUN_COMMAND = 'sample'
 CLEAN_COMMAND = 'clean'
@@ -25,6 +28,8 @@ MELCOR_PATH = os.path.join(EXEC_DIR, 'melcor-fusion-186_bdba')
 
 EDF_PATH = os.path.join(EXEC_DIR, 'VARIABLES.DAT')
 
+AUX_CVS = ['CV001']
+
 ACTIONS = {
     0: (0, 0),
     1: (0.1, 0),
@@ -35,20 +40,30 @@ ACTIONS = {
     6: (-0.1, -0.1)
 }
 
+
 class SampleEnv(gym.Env):
+    '''
+    A sample MELCOR environment class.
+    '''
+
     def __init__(self, obs_size, n_actions):
         '''
         Environment constructor.
         '''
         low_obs = np.zeros(obs_size)
         high_obs = np.inf * np.ones(obs_size)
+
+        # So many observed variables as CVs representing HVAC served rooms
         self.observation_space = spaces.Box(low=low_obs, high=high_obs)
 
-        low_action = -np.inf * np.ones(n_actions)
-        high_action = np.inf * np.ones(n_actions)
-        self.action_space = spaces.Box(low=low_action, high=high_action)
+        # So many actions (between 0 and 6) as CVs representing HVAC served rooms
+        # Each action is an (overpressure, underpressure) increment/decrement tuple
+        self.action_space = spaces.MultiDiscrete([n_actions] * obs_size)
 
-    def step(self, action):
+        self.n_cvs = obs_size
+        self.toolkit = Toolkit(INPUT_PATH)
+
+    def step(self, actions):
         '''
         This function defines the execution and effect of an action on the environment. It consists of the following steps:
             - Modifies the scalar values by which the supply velocity is multiplied in cases of overpressure and underpressure.
@@ -58,24 +73,46 @@ class SampleEnv(gym.Env):
             - Check if all pressures are at an acceptable level. If so, the episode is terminated.
         '''
 
-        # TO-DO: editar input con nuevos sobrepresion/depresion values
-        # para cada CV_i:
-            # obtener CFs de sobrepresion y depresion asociados
-            # aplicar (sobrepresion, depresion) += ACTIONS[action_i]
+        # Get HVAC-served CVs
+        hvac_served_cvs = []
+        for cv in self.toolkit.get_cv_list():
+            if cv.get_id() not in AUX_CVS:
+                hvac_served_cvs.append(cv)
 
-        # Relanzar ejecucion de simulador
+        # Update supply CFs
+        for i, cv in enumerate(hvac_served_cvs):
+            action = ACTIONS[actions[i]]
+            fls_connected = self.toolkit.get_fl_connections(cv.get_id())
+            supply_fls = filter(lambda fl: fl.get_field(
+                'FLNAME').startswith('SUPPLY'), fls_connected)
+            for fl in supply_fls:
+                cfs_connected = self.toolkit.get_connected_cfs(fl.get_id())
+                overpressure_cfs = [cf for cf in cfs_connected if cf.get_field(
+                    'CFNAME') == 'OVERPRESSURE']
+                underpressure_cfs = [cf for cf in cfs_connected if cf.get_field(
+                    'CFNAME') == 'UNDERPRESSURE']
+                overpressure_cfs[0].update_field('CFSCAL', float(
+                    overpressure_cfs[0].get_field('CFSCAL')) + action[0])
+                underpressure_cfs[0].update_field('CFSCAL', float(
+                    underpressure_cfs[0].get_field('CFSCAL')) + action[1])
+                
+                # [!] TO-FIX
+                # self.toolkit.update_object(underpressure_cfs[0])
+                # self.toolkit.update_object(overpressure_cfs[0])
+
+        # Rerun MELCOR
         subprocess.run(['make', CLEAN_ALL_COMMAND], cwd=EXEC_DIR)
         subprocess.run(['make', RUN_COMMAND], cwd=EXEC_DIR)
 
-        # Obtener nueva observacion
+        # Get new observation
         new_obs = edf.make_observation(
             input_file=INPUT_PATH, edf_file=EDF_PATH)
 
-        # Calcular recompensa (numero de presiones en su rango / con variacion minima)
+        # Get reward (pressure compliance accomplished)
         reward = rewards.get_pressure_compliance(new_obs)
 
-        # Consultar fin de episodio (todas las presiones en rango aceptable)
-        done = (reward == self.obs_size)
+        # Check episode termination
+        done = (reward == self.n_cvs)
 
         return new_obs, reward, done, False, {}
 
