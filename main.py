018cfd5bfbbdc90a87801c03d48edc6f86f1f54c
Antonio <antomanjavacas@gmail.com>
2023-06-07 16:22:14 +0200

    First training

--- main.py
+++ main.py
@@ -10,23 +10,26 @@ from stable_baselines3 import PPO
 N_BRANCHES = 1
 N_ROOMS = 2
 
-def train():
-    env = gym.make('hvac-v0', n_obs=N_ROOMS, n_actions=N_BRANCHES, control_horizon=10, max_deviation=20)
-    model = PPO('MlpPolicy', env, verbose=1)
-    model.learn(total_timesteps=25000)
-    model.save('model-ppo')
 
-def run():
-    env = gym.make('hvac-v0', n_obs=N_ROOMS, n_actions=N_BRANCHES, control_horizon=10, max_deviation=20)
-    obs, info = env.reset()
-    terminated = False
-    truncated = False
-    while not terminated and not truncated:
+def train(env):
+    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log='./tensorboard/')
+    model.learn(total_timesteps=25000)
+    model.save('./models/model-ppo')
+
+
+def run(env):
+    model = PPO.load('./models/model-ppo')
+    obs, _ = env.reset()
+    while True:
+        action, _ = model.predict(obs)
+        obs, reward, _, _, info = env.step(action)
         env.render()
-        obs, reward, truncated, terminated, info = env.step(env.action_space.sample())
-        print(f'info={info}\nreward={reward}, obs={obs}, terminated={terminated}, truncated={truncated}\n==============\n')
-    env.close()
+        print(f'\n===========\n{info}\nReward={reward}\n===========\n')
+
 
 if __name__ == '__main__':
-    # run()
-    train()
+    env = gym.make('hvac-v0', n_obs=N_ROOMS, n_actions=N_BRANCHES,
+                   control_horizon=10, max_deviation=20)
+    train(env)
+    run(env)
+    env.close()
