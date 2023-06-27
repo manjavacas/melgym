8ef842a83aac263411f7dfffe1ec807984850631
Antonio <antomanjavacas@gmail.com>
2023-06-27 15:31:21 +0200

    Updated branch.inp with leaks

--- Makefile
+++ Makefile
@@ -4,9 +4,9 @@ rand:
 run:
 	./run_experiment.py -conf cfg.json
 gen: clean
-	./melgym/exec/MELGEN ow=o i=./melgym/data/base.inp
+	./melgym/exec/MELGEN ow=o i=./melgym/data/branch.inp
 cor:
-	./melgym/exec/MELCOR ./melgym/data/base.inp
+	./melgym/exec/MELCOR ./melgym/data/branch.inp
 clean:
 	rm -rf ./melgym/out/*
 	rm -rf ./metrics/
--- rand_controller.py
+++ rand_controller.py
@@ -8,24 +8,31 @@ MAX_DEVIATION = 20
 CHECK_DONE_TIME = 1_000
 CONTROL_HORIZON = 5
 
+
+def summary(episode, action, obs, reward, info):
+    print(''.join(['[ACTION] ', str(action), '\n', 80 * '-', '\n[EPISODE] ', str(episode),
+                   '\n[REWARD] ', str(reward), '\n[OBSERVATION] ', str(obs),
+                   '\n[TIME] ', str(info['time']), '\n[PRESSURES] ', str(
+                       info['pressures']),
+                   '\n[DISTANCES] ', str(info['distances']), '\n', 80 * '-']))
+
+
 def rand_control(env):
     obs, _ = env.reset()
     done = False
     truncated = False
-    i = 1
+    n_episode = 1
     while not done and not truncated:
-        action = env.action_space.sample()
-        # action = [7.785]
-        print('Action = ', str(action))
+        # action = env.action_space.sample()
+        action = [10.0]
         obs, reward, truncated, done, info = env.step(action)
-        print(''.join([80 * '-', '\nEpisode/ ', str(i), '\nReward/ ', str(reward),
-              '\nObservation/ ', str(obs), '\nInfo/ ', str(info), '\n', 80 * '-']))
+        summary(n_episode, action, obs, reward, info)
         env.render()
-        i += 1
+        n_episode += 1
 
 
 if __name__ == '__main__':
-    env = gym.make('branch-nl-v0', control_horizon=CONTROL_HORIZON, check_done_time=CHECK_DONE_TIME,
+    env = gym.make('branch-v0', control_horizon=CONTROL_HORIZON, check_done_time=CHECK_DONE_TIME,
                    max_deviation=MAX_DEVIATION, render_mode='distances')
     rand_control(env)
     env.close()
