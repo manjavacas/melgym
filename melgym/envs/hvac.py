Fixed obs/action normalization

melgym/envs/hvac.py
@@ -18,7 +18,7 @@ class EnvHVAC(Env):
     """
     metadata = {'render_modes': ['pressures', 'distances']}
 
-    def __init__(self, input_file, n_actions, controlled_cvs, control_horizon=5, check_done_time=1_000, max_deviation=20, max_vel=10, render_mode=None, time_bt_frames=0.1):
+    def __init__(self, input_file, n_actions, controlled_cvs, control_horizon=5, check_done_time=1_000, max_deviation=40, min_velocity=0, max_velocity=10, render_mode=None, time_bt_frames=0.1):
         """
         Class constructor.
 
@@ -29,7 +29,8 @@ class EnvHVAC(Env):
             control_horizon (int, optional): number of simulation cycles between actions. Defaults to 5.
             check_done_time (int, optional): simulation time allowed before evaluating the termination condition. Defaults to 1000.
             max_deviation (float, optional): maximum distance allowed from original pressures. Defaults to 20 (Pa).
-            max_vel (float): maximum value for control actions. Defaults to 10 (m/s).
+            min_velocity (float): minimum value for control actions. Defaults to 0 (m/s).
+            max_velocity (float): maximum value for control actions. Defaults to 10 (m/s).
             render_mode (str): render option.
         """
 
@@ -39,10 +40,6 @@ class EnvHVAC(Env):
 
         self.observation_space = spaces.Box(
             low=np.zeros(n_obs), high=np.inf * np.ones(n_obs), dtype=np.float32)
-
-        # Real action space [0, max_vel]
-        self.real_action_space = spaces.Box(
-            low=np.zeros(n_actions), high=max_vel * np.ones(n_actions), dtype=np.float32)
 
         # Normalized action space [-1, 1]
         self.action_space = spaces.Box(
@@ -57,6 +54,8 @@ class EnvHVAC(Env):
         self.control_horizon = control_horizon
         self.check_done_time = check_done_time
         self.max_deviation = max_deviation
+        self.max_velocity = max_velocity
+        self.min_velocity = min_velocity
 
         # Aux variables
         self.n_steps = 0
@@ -143,6 +142,9 @@ class EnvHVAC(Env):
             bool: truncated flag. It will be True if pressures keep stable until the imposed time step limit.
             dict: additional step information (last recorded time and pressures).
         """
+
+        # Denormalize action
+        action = self.__denorm_action(action)
 
         # Input edition
         self.__update_time()
@@ -251,6 +253,18 @@ class EnvHVAC(Env):
             self.n_steps += 1
         else:
             raise Exception('No reset has been done before step')
+
+    def __denorm_action(self, action):
+        """
+        Maps [-1,1] actions to [min_velocity, max_velocity]
+
+        Args:
+            action (np.array): current normalized action.
+
+        Returns:
+            np.array: denormalized action.
+        """
+        return ((action + 1) * ((self.max_velocity - self.min_velocity) / 2)) + self.min_velocity
 
     def __get_last_record(self):
         """

melgym/utils/wrappers.py
@@ -1,33 +1,30 @@
+from gymnasium import ObservationWrapper, ActionWrapper
+from gymnasium import spaces
+
 import numpy as np
 
-from gymnasium import ActionWrapper
+
+class NormalizedObservations(ObservationWrapper):
+    def __init__(self, env, min_val, max_val):
+        super(ObservationWrapper, self).__init__(env)
+        self.min_val = min_val
+        self.max_val = max_val
+        self.observation_space = spaces.Box(
+            low=0, high=1, shape=env.observation_space.shape, dtype=np.float32)
+
+    def observation(self, observation):
+        # Normalize to [0, 1]
+        return (observation - self.min_val) / (self.max_val - self.min_val)
 
 
-class DenormaliseActionsWrapper(ActionWrapper):
-    def __init__(self, env):
-        super().__init__(env)
-        self.action_space = env.action_space
-        self.real_action_space = env.real_action_space
+class DenormalizedActions(ActionWrapper):
+    def __init__(self, env, min_val, max_val):
+        super(ActionWrapper, self).__init__(env)
+        self.min_val = min_val
+        self.max_val = max_val
+        self.action_space = spaces.Box(
+            low=min_val, high=max_val, shape=env.action_space.shape, dtype=np.float32)
 
     def action(self, action):
-        """
-        Converts normalised actions to their corresponding real values. 
-        If the actions are not normalised, they are returned without applying any change.
-
-        Args:
-            action (np.array): action array.
-
-        Returns:
-            np.array: transformed action values.
-        """
-
-        tr_action = np.zeros_like(action)
-
-        for i, value in enumerate(action):
-            norm_range = self.action_space.high[i] - self.action_space.low[i]
-            real_range = self.real_action_space.high[i] - \
-                self.real_action_space.low[i]
-            tr_action[i] = self.real_action_space.low[i] + \
-                (value - self.action_space.low[i]) * real_range / norm_range
-
-        return tr_action
+        # Map [-1, 1] to [min_val, max_val]
+        return (action + 1) * ((self.max_val - self.min_val) / 2) + self.min_val

rand_controller.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
 
+import numpy as np
+import random
 import melgym
 import gymnasium as gym
 
-import random
-
-MAX_DEVIATION = 20
-CHECK_DONE_TIME = 1_000
-CONTROL_HORIZON = 5
-
-
-def summary(episode, action, obs, reward, info):
-    print(''.join(['[ACTION] ', str(action), '\n', 80 * '-', '\n[EPISODE] ', str(episode),
-                   '\n[REWARD] ', str(reward), '\n[OBSERVATION] ', str(obs),
-                   '\n[TIME] ', str(info['time']), '\n[PRESSURES] ', str(
-                       info['pressures']),
-                   '\n[DISTANCES] ', str(info['distances']), '\n', 80 * '-']))
+from melgym.utils.aux import summary
 
 
 def rand_control(env):
@@ -24,8 +14,8 @@ def rand_control(env):
     truncated = False
     n_episode = 1
     while not done and not truncated:
-        # action = env.action_space.sample()
-        action = [random.choice([7., 9., 10.])]
+        action = env.action_space.sample()
+        # action = np.array([-0.85])
         obs, reward, truncated, done, info = env.step(action)
         summary(n_episode, action, obs, reward, info)
         env.render()
@@ -33,7 +23,7 @@ def rand_control(env):
 
 
 if __name__ == '__main__':
-    env = gym.make('simple-v0', control_horizon=CONTROL_HORIZON, check_done_time=CHECK_DONE_TIME,
-                   max_deviation=MAX_DEVIATION, render_mode='pressures')
+    env = gym.make('simple-v0', render_mode='pressures')
+
     rand_control(env)
     env.close()

run_experiment.py
@@ -4,10 +4,13 @@ import argparse
 import json
 import copy
 
+import numpy as np
 import gymnasium as gym
 
+from gymnasium.wrappers.normalize import NormalizeObservation
+
 from melgym.utils.callbacks import MetricsCallback
-from melgym.utils.wrappers import DenormaliseActionsWrapper
+from melgym.utils.aux import summary
 
 from stable_baselines3 import PPO, DDPG, TD3, SAC
 from stable_baselines3.common.callbacks import EvalCallback
@@ -20,22 +23,27 @@ ALGORITHMS = {
 }
 
 
-def summary(episode, action, obs, reward, info):
+def get_config():
     """
-    Prints an episode summary.
+    Parses the experiment json configuration file.
 
-    Args:
-        episode (int): current episode number.
-        action (np.array): last performed action.
-        obs (np.array): last observation.
-        reward (float): last reward.
-        info (dict): additional information.
+    Returns:
+        dict: experiment configuration.
     """
-    print(''.join(['[ACTION] ', str(action), '\n', 80 * '-', '\n[EPISODE] ', str(episode),
-                   '\n[REWARD] ', str(reward), '\n[OBSERVATION] ', str(obs),
-                   '\n[TIME] ', str(info['time']), '\n[PRESSURES] ', str(
-                       info['pressures']),
-                   '\n[DISTANCES] ', str(info['distances']), '\n', 80 * '-']))
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--configuration',
+        '-conf',
+        required=True,
+        type=str,
+        dest='configuration',
+        help='Path to experiment configuration (JSON file)'
+    )
+    args = parser.parse_args()
+
+    with open(args.configuration) as json_config:
+        config = json.load(json_config)
+    return config
 
 
 def train(env, config):
@@ -65,11 +73,7 @@ def train(env, config):
 
     # Callbacks
     eval_callback = EvalCallback(
-        env_eval, best_model_save_path='./best_models/' + experiment_id + '/', eval_freq=eval_freq, n_eval_episodes=n_eval_episodes)
-
-    # Action denormalisation
-    env = DenormaliseActionsWrapper(env)
-    env_eval = DenormaliseActionsWrapper(env_eval)
+        env_eval, best_model_save_path='./best_models/' + experiment_id + '/', eval_freq=eval_freq, n_eval_episodes=n_eval_episodes, deterministic=True)
 
     # Model configuration
     if config['algorithm']['name'] in ALGORITHMS:
@@ -98,29 +102,19 @@ def test(env, config):
     truncated = False
     i = 1
     while not done and not truncated:
-        action, _ = model.predict(obs)
+        action, _ = model.predict(obs, deterministic=True)
         obs, reward, truncated, done, info = env.step(action)
         summary(i, action, obs, reward, info)
         env.render()
         i += 1
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--configuration',
-    '-conf',
-    required=True,
-    type=str,
-    dest='configuration',
-    help='Path to experiment configuration (JSON file)'
-)
-args = parser.parse_args()
-
-with open(args.configuration) as json_config:
-    config = json.load(json_config)
-
+config = get_config()
 env = gym.make(config['env']['name'], **config['env']
-               ['params'], render_mode='distances')
+               ['params'], render_mode='pressures')
+
+# Normalization wrapper
+env = NormalizeObservation(env)
 
 train(env, config)
 test(env, config)