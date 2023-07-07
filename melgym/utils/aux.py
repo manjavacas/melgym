6a9663ac5ea14c5b2e9b1482cbc33a320fd81f5a
Antonio <antomanjavacas@gmail.com>
2023-07-07 14:52:26 +0200

    Updated experiments settings

--- melgym/utils/aux.py
+++ melgym/utils/aux.py
@@ -1,9 +1,9 @@
 def summary(episode, action, obs, reward, info, min_velocity=0, max_velocity=10):
     """
-    Prints an episode summary.
+    Prints a step summary.
 
     Args:
-        episode (int): current episode number.
+        step (int): current step number.
         action (np.array): last performed action.
         obs (np.array): last observation.
         reward (float): last reward.
@@ -11,7 +11,7 @@ def summary(episode, action, obs, reward, info, min_velocity=0, max_velocity=10)
         max_velocity (float): max action value. Defaults to 10.
         min_velocity (float): min action value. Defaults to 0.
     """
-    print(''.join(['\n', 80 * '-', '\nEpisode ', str(episode), 2 * '\n',
+    print(''.join(['\n', 80 * '-', '\nStep ', str(episode), 2 * '\n',
                    '[ACTION (norm)] ', str(action), '\n',
                    '[ACTION (real)] ', str(
                        ((action + 1) * ((max_velocity - min_velocity) / 2)) + min_velocity), '\n',
