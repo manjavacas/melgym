9a056c1142d477fbd8c890a6c5efbaf1f66c5233
Antonio <antomanjavacas@gmail.com>
2023-11-07 11:55:04 +0100

    Updated branch-v0 with new leakages

--- rand_controller.py
+++ rand_controller.py
@@ -25,6 +25,7 @@ def rand_control(env):
         while not done and not truncated:
             env.render()
             action = env.action_space.sample()
+            # action = np.array([0.5])
             obs, reward, truncated, done, info = env.step(action)
             summary(n_steps, action, obs, reward, info)
             n_steps += 1
