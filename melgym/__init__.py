d01f2d8c5a7aaeab4be35b58dda7da1aeb79a6df
Antonio <antomanjavacas@gmail.com>
2023-11-30 13:55:59 +0100

    Removed deprecated models

--- melgym/__init__.py
+++ melgym/__init__.py
@@ -11,36 +11,6 @@ register(
 )
 
 register(
-    id='simple-v0',
-    entry_point='melgym.envs:EnvHVAC',
-    kwargs={
-        'input_file': '(deprecated)-simple.inp',
-        'n_actions': 1,
-        'controlled_cvs': ['CV001', 'CV006']
-    }
-)
-
-register(
-    id='branch-v0',
-    entry_point='melgym.envs:EnvHVAC',
-    kwargs={
-        'input_file': '(deprecated)-branch.inp',
-        'n_actions': 1,
-        'controlled_cvs': ['CV086', 'CV083', 'CV026', 'CV011', 'CV006', 'CV001']
-    }
-)
-
-register(
-    id='hvac-v0',
-    entry_point='melgym.envs:EnvHVAC',
-    kwargs={
-        'input_file': '(deprecated)-hvac.inp',
-        'n_actions': 1,
-        'controlled_cvs': ['CV001', 'CV006', 'CV011']
-    }
-)
-
-register(
     id='branch0-v0',
     entry_point='melgym.envs:EnvHVAC',
     kwargs={
