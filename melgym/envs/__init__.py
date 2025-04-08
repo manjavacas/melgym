9b081cda9de1120ad8908c1ebbd0b4d7eb411e42
Antonio <antomanjavacas@gmail.com>
2025-04-08 16:25:49 +0200

    Fixed docs

--- melgym/envs/__init__.py
+++ melgym/envs/__init__.py
@@ -4,4 +4,5 @@ Control environments provided by MELGYM.
 Custom environments must inherit from the base MelcorEnv class.
 """
 
-from melgym import *
+from melgym.envs.pressure import PressureEnv
+from melgym.envs.melcor import MelcorEnv
