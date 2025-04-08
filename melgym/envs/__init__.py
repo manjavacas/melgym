f9aab8c22c5ef94778f00a29a4caecb9908192de
Antonio <antomanjavacas@gmail.com>
2025-04-08 16:59:25 +0200

    Fixed docs

--- melgym/envs/__init__.py
+++ melgym/envs/__init__.py
@@ -3,3 +3,6 @@ Control environments provided by MELGYM.
 
 Custom environments must inherit from the base MelcorEnv class.
 """
+
+from . import melcor
+from . import pressure
--- melgym/utils/__init__.py
+++ melgym/utils/__init__.py
@@ -2,3 +2,5 @@
 Constants and utilities.
 """
 
+from . import constants
+from . import exceptions
